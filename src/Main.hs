6c5d97d53e5792cc8a09773bcb0dc61aad865e96
Etienne Chartrand <chartrand.etienne@gmail.com>
2015-05-14 11:04:40 -0400

    got some queries running an stuff

--- src/Main.hs
+++ src/Main.hs
@@ -24,6 +24,7 @@ import qualified Snap.Snaplet as Snaplet
 import qualified Snap.Snaplet.Session.Backends.CookieSession as CookieSession
 import Data.ByteString (ByteString)
 import Data.ByteString.Char8 (pack)
+import Data.ByteString.Lazy (toStrict)
 import qualified Data.ByteString as ByteString
 import           Data.ByteString (ByteString)
 import qualified Snap as S
@@ -35,15 +36,13 @@ import qualified Data.Text as Text
 import qualified MyDatabase as MyDatabase
 
 import qualified Data.Aeson as Aeson
+import Data.Aeson ((.=))
 
 import qualified Control.Monad.State as MonadState
 import qualified Control.Monad.Trans.Reader as MonadReader
 import qualified Control.Monad.Logger as MonadLogger
 import qualified Control.Monad.Trans.Resource as MonadResource
 import qualified Snap.Snaplet.PostgresqlSimple as PSql
--- import Text.Digestive.Blaze.Html5
--- import Text.Digestive.Happstack
--- import Text.Digestive.Util
 
 data App = App 
     { _sess ::  S.Snaplet Sess.SessionManager
@@ -52,17 +51,20 @@ data App = App
 
 Lens.makeLenses ''App
 
-TH.share [TH.mkPersist TH.sqlSettings, TH.mkMigrate "migrateAll"] [TH.persistLowerCase|
-Usr
-    login String
-    deriving Show
-Link
-    url String
-    authorId UsrId
-    deriving Show
-|]
-
 data LinkInput = LinkInput { url :: String }
+
+data Usr = Usr { usr_id :: Int, usr_login :: String } deriving (Show)
+
+data Post = Post { post_id :: Int, post_url :: Text.Text, post_title :: Text.Text, post_usr_fk :: Int } deriving (Show)
+
+instance Aeson.ToJSON Post where
+    toJSON (Post post_id post_url post_title post_usr_fk) = Aeson.object ["url" .= post_url, "title" .= post_title]
+
+instance PSql.FromRow Usr where
+    fromRow = Usr CApp.<$> PSql.field CApp.<*> PSql.field
+
+instance PSql.FromRow Post where
+    fromRow = Post CApp.<$> PSql.field CApp.<*> PSql.field CApp.<*> PSql.field CApp.<*> PSql.field
 
 instance Aeson.FromJSON LinkInput where
     parseJSON (Aeson.Object v)  = 
@@ -105,16 +107,16 @@ maybe_user name = do
     --             [Sqlite.Entity uh ah]   -> return $ Just $ Sqlite.Entity uh ah
     --             _   -> return Nothing
 
-    onePossibleUser <- S.with db $ PSql.query_ "select * from login" :: MyHandler [(Int, Text.Text)]
+    onePossibleUser <- S.with db $ PSql.query_ "select * from usr" :: MyHandler [Usr]
     case onePossibleUser of 
-        [(id, login)] -> return $ Just (id, login)
+        [Usr id login] -> return $ Just $ Usr id login
         _ -> return Nothing
 
 authHandler :: MyHandler ()
 authHandler = do
     maybe_name <- S.getPostParam "user"
     let name = decodeUtf8 $ fromMaybe "" maybe_name
-    onePossibleUser <- S.with db $ PSql.query_ "select * from login" :: MyHandler [(Int, Text.Text)]
+    onePossibleUser <- S.with db $ PSql.query_ "select * from usr" :: MyHandler [(Int, Text.Text)]
     case onePossibleUser of 
         [(id, login)] -> S.with sess $ Sess.setInSession "user" login >> Sess.commitSession
         _ -> return ()
@@ -126,7 +128,7 @@ authHandler = do
     S.redirect "/"
     
 
-auth :: ((Int, Text.Text) -> MyHandler ()) -> MyHandler ()
+auth :: (Usr -> MyHandler ()) -> MyHandler ()
 auth success = do
     maybe_sess <- S.with sess $ Sess.getFromSession "user"
     case maybe_sess of 
@@ -137,14 +139,15 @@ auth success = do
                 _ -> S.writeBS "nope here"
         _ -> S.writeBS "nope there"
 
-    S.writeBS "nope"
 
-pinsHandler :: (Int, Text.Text) -> MyHandler () 
+pinsHandler :: Usr -> MyHandler () 
 pinsHandler user = S.method S.GET $ do
-    S.writeBS $ ByteString.concat ["so yeah; ", "fuck persistent"]
+    posts <- S.with db $ PSql.query "select * from post where usr_id = ?" (PSql.Only $ usr_id user) :: MyHandler [Post]
+    let to_string = toStrict $ Aeson.encode posts
+    S.writeBS $ ByteString.concat ["so yeah; ", "fuck persistent: ", to_string]
     -- S.writeBS "yeah"
 
-addPinHandler :: (Int, Text.Text) -> MyHandler ()
+addPinHandler :: Usr -> MyHandler ()
 addPinHandler user = S.method S.POST $ do
     S.writeBS "okay"
 
