f01407ed198b25e4e58f7aa2fd2feeba8d91b8fb
Etienne Chartrand <chartrand.etienne@gmail.com>
2015-06-03 23:41:08 -0400

    notes

--- gulpfile.js
+++ gulpfile.js
@@ -9,35 +9,94 @@ var source = require('vinyl-source-stream');
 var gutil = require('gulp-util');
 
 gulp.task('browserify', function() {
-    var bundler = browserify({
-        entries: ["./frontend/src/Main.jsx"],
-        debug: true, // Gives us sourcemapping
-        cache: {}, packageCache: {}, fullPaths: true // Requirement of watchify
-    }).transform('reactify', {stripTypes: true});
-    var watcher  = watchify(bundler);
 
-    var dashboard = watcher
-	// When any files update
-    .on('update', function () {
-        var updateStart = Date.now();
-        console.log('Updating!');
-        watcher.bundle()
+	var dashboard_bundler = browserify({
+		entries: ["./frontend/src/Main.jsx"],
+		debug: true, // Gives us sourcemapping
+		cache: {}, packageCache: {}, fullPaths: true // Requirement of watchify
+	}).transform('reactify', {stripTypes: true});
+	var dashboard_watcher = watchify(dashboard_bundler);
+
+	var dashboard = dashboard_watcher
+	.on('update', function () {
+		var updateStart = Date.now();
+		console.log('Updating!');
+		dashboard_watcher.bundle()
 		.on('error', function(e) {
 			gutil.log(gutil.colors.red('Bundle error:', e.message));
 		})
 // Create new bundle that uses the cache for high performance
-        .pipe(source('app.js'))
+		.pipe(source('app.js'))
 		// This is where you add uglifying etc.
 		.pipe(gulp.dest("./frontend/dist"));
-        console.log('Updated!', (Date.now() - updateStart) + 'ms');
-    })
+		console.log('Updated!', (Date.now() - updateStart) + 'ms');
+	})
 	// Create the initial bundle when starting the task
-    .bundle()
+	.bundle()
 	.on('error', function(e) {
 		gutil.log(gutil.colors.red('Bundle error:', e.message));
 	})
-    .pipe(source('app.js'))
-    .pipe(gulp.dest("./frontend/dist"));
+	.pipe(source('app.js'))
+	.pipe(gulp.dest("./frontend/dist"));
+
+
+	var background_bundler = browserify({
+		entries: ["./extension/src/background.js"],
+		debug: true, // Gives us sourcemapping
+		cache: {}, packageCache: {}, fullPaths: true // Requirement of watchify
+	}).transform('reactify', {stripTypes: true});
+	var background_watcher = watchify(background_bundler);
+
+	var background = background_watcher
+	.on('update', function () {
+		var updateStart = Date.now();
+		console.log('Updating!');
+		background_watcher.bundle()
+		.on('error', function(e) {
+			gutil.log(gutil.colors.red('Bundle error:', e.message));
+		})
+// Create new bundle that uses the cache for high performance
+		.pipe(source('background.js'))
+		// This is where you add uglifying etc.
+		.pipe(gulp.dest("./extension"));
+		console.log('Updated!', (Date.now() - updateStart) + 'ms');
+	})
+	// Create the initial bundle when starting the task
+	.bundle()
+	.on('error', function(e) {
+		gutil.log(gutil.colors.red('Bundle error:', e.message));
+	})
+	.pipe(source('background.js'))
+	.pipe(gulp.dest("./extension"));
+
+	var content_bundler = browserify({
+		entries: ["./extension/src/content.js"],
+		debug: true, // Gives us sourcemapping
+		cache: {}, packageCache: {}, fullPaths: true // Requirement of watchify
+	}).transform('reactify', {stripTypes: true});
+	var content_watcher = watchify(content_bundler);
+
+	var content = content_watcher
+	.on('update', function () {
+		var updateStart = Date.now();
+		console.log('Updating!');
+		content_watcher.bundle()
+		.on('error', function(e) {
+			gutil.log(gutil.colors.red('Bundle error:', e.message));
+		})
+// Create new bundle that uses the cache for high performance
+		.pipe(source('content.js'))
+		// This is where you add uglifying etc.
+		.pipe(gulp.dest("./extension"));
+		console.log('Updated!', (Date.now() - updateStart) + 'ms');
+	})
+	// Create the initial bundle when starting the task
+	.bundle()
+	.on('error', function(e) {
+		gutil.log(gutil.colors.red('Bundle error:', e.message));
+	})
+	.pipe(source('content.js'))
+	.pipe(gulp.dest("./extension"));
 
 
 });
